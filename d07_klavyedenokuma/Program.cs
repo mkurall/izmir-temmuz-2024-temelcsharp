c8180977df284fc356b870ee998d71f4b54fe7d4
Mustafa KURAL <mustafakural@outlook.com>
2024-07-01 14:51:17 +0300

    iki sayıyı topla

---
+++ d07_klavyedenokuma/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Adınızı girin: ");//ekrana çıktı
+var ad = Console.ReadLine();//klavyeden okumak(enter tuşu basılınca)
+
+Console.Write("Soyadınızı girin: ");
+var soyad = Console.ReadLine();
+
+Console.WriteLine("Hoşgeldiniz, " + ad + " " + soyad);
---
+++ d08_iki_sayi_topla/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Bu uygulama klavyeden " + 
+"girilen iki tam sayıyı toplar");
+
+Console.Write("1.Sayıyı Girin:");
+var s1 = Console.ReadLine();
+
+Console.Write("2.Sayıyı Girin:");
+var s2 = Console.ReadLine();
+
+int sayi1 = Convert.ToInt32(s1);//elimde artık sayı var
+int sayi2 = Convert.ToInt32(s2);//elimde artık sayı var
+//+ karakteri metinler üzerrinde birleştirme yapar
+//+ karakteri sayılar üzerinde toplama yapar
+//Console.WriteLine("Sayıların Toplamı = " + (sayi1 + sayi2));
+int toplam = sayi1 + sayi2;
+Console.WriteLine("Sayıların Toplamı = " + toplam);
