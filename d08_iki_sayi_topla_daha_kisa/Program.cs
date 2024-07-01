aac2958f187a05e3c5cae54aa01817ce69efc603
Mustafa KURAL <mustafakural@outlook.com>
2024-07-01 15:36:55 +0300

    string formatlama

--- d08_iki_sayi_topla_daha_kisa/Program.cs
+++ d08_iki_sayi_topla_daha_kisa/Program.cs
@@ -7,5 +7,4 @@ var s1 = Convert.ToInt32(Console.ReadLine());//  int <- ToInt32(string)
 Console.Write("2.Sayıyı Girin:");
 var s2 = Convert.ToInt32(Console.ReadLine());
 
-
 Console.WriteLine("Sayıların Toplamı = " + (s1 + s2));
---
+++ d09_metin_bicimlendirme/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Adınızı girin:");
+var ad = Console.ReadLine();
+Console.Write("Soyadınızı girin:");
+var soyad = Console.ReadLine();
+
+Console.Write("Cinsiyetinizi girin[E/K]:");
+var cinsiyet = Console.ReadLine();
+Console.Write("Doğum yılınızı girin:");
+var dogumYili = Convert.ToInt32(Console.ReadLine());//------int
+Console.Write("Kilonuzu girin:");
+var kilo = Convert.ToDouble(Console.ReadLine());//----------double
+Console.Write("Boyunuzu girin:");
+var boy = Convert.ToDouble(Console.ReadLine());//-----------double
+
+Console.WriteLine("\\Kişisel \"Size Özel\" Bilgileriniz\n--------------------------------");//\n bir alt satıra geç
+Console.WriteLine($"Ad Soyad     : {ad} {soyad}");//string interpolation
+Console.WriteLine($"Cinsiyetiniz : {cinsiyet}");//string interpolation
+Console.WriteLine($"Doğum Yılınız: {dogumYili}");//string interpolation
+Console.WriteLine($"Yaşınız      : {2024 - dogumYili}");//string interpolation
+Console.WriteLine($"Kilonuz      : {kilo,10:f2}");//string interpolation
+Console.WriteLine($"Boyunuz      : {boy,10:f2}");//string interpolation
