28def094c508c73d014a4c047e7c31c9156e4805
Mustafa KURAL <mustafakural@outlook.com>
2024-07-01 16:54:12 +0300

    typeof ve sizeof

---
+++ d11_dairenin_alani_cevresi/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Soru: Klavyeden girilen dairenin yarıçap uzunluğuna göre
+alanını ve çevresini hesaplayıp sonucu
+ekranda gösteren uygulama
+
+1.Aşama = Klavyeden değer oku
+2.Aşama = İşlem Yap
+3.Aşama = Sonuçları göster
+*/
+
+const double pi = 3.14;//const sonradan değişkenin değerinin değişmeyeceğini belirtir
+//1.Aşama
+Console.Write("Dairenin yarı çapını gir:");
+double r = Convert.ToDouble(Console.ReadLine());
+
+//pi = 0; burada hata alırız çünkü pi sabit
+
+//2.Aşama
+double alan = pi * r * r;
+double cevre = 2 * pi * r;
+
+//3.Aşama
+Console.WriteLine($"Yarıçapı {r} olan Dairenin Alanı = {alan:f2}");
+Console.WriteLine($"Yarıçapı {r} olan Dairenin Çevresi = {cevre:f2}");
---
+++ d12_sizeof_typeof/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("İLKEL DEĞİŞKEN TİPLERİ\n---------------------\n\n");
+Console.WriteLine("Veri Türü           Kapladığı Alan");
+//typeof verinin tip adını verir
+//sizeof tipin boyutunu verir
+
+Console.WriteLine($"{typeof(byte),-20}{sizeof(byte)}");
+Console.WriteLine($"{typeof(sbyte),-20}{sizeof(sbyte)}");
+Console.WriteLine($"{typeof(short),-20}{sizeof(short)}");
+Console.WriteLine($"{typeof(ushort),-20}{sizeof(ushort)}");
+Console.WriteLine($"{typeof(int),-20}{sizeof(int)}");
+Console.WriteLine($"{typeof(uint),-20}{sizeof(uint)}");
+Console.WriteLine($"{typeof(long),-20}{sizeof(long)}");
+Console.WriteLine($"{typeof(ulong),-20}{sizeof(ulong)}");
+Console.WriteLine($"{typeof(float),-20}{sizeof(float)}");
+Console.WriteLine($"{typeof(double),-20}{sizeof(double)}");
+Console.WriteLine($"{typeof(decimal),-20}{sizeof(decimal)}");
+Console.WriteLine($"{typeof(bool),-20}{sizeof(bool)}");
+Console.WriteLine($"{typeof(char),-20}{sizeof(char)}");
+
