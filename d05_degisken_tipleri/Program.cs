53b583f2a4fe01baa8ad0e2fe5e6ff4cd046711d
Mustafa KURAL <mustafakural@outlook.com>
2024-07-01 14:22:24 +0300

    tip doüşümleri

--- d05_degisken_tipleri/Program.cs
+++ d05_degisken_tipleri/Program.cs
@@ -1,9 +1,12 @@
 ﻿string adSoyad = "Mustafa KURAL";
 short dogumYili = 1981;
 float ortalama = 3.66f;//varsayılan tip double - sol taraf ile sağ taraf aynı tipten olmalı
-char cinsiyet  ='E';
+char cinsiyet = 'E';
 bool basariliMi = true;
 double boy = 1.70;
+byte s1=5, s2=8, s3; //aynı satırda aynı türden değişkenler tanımlanabilir
+
+s3 = 10;
 
 Console.WriteLine(adSoyad);
 Console.WriteLine(dogumYili);
---
+++ d06_tip_donusumleri/Program.cs
@@ -0,0 +1,19 @@
+﻿byte a = 189;
+int b = 257;
+string sayi = "1234";//metin
+
+byte c = (byte)b;//Açık Dönüşüm  byte <- int
+int d = a;//Örtük dönüşüm  int <- byte
+int s = Convert.ToInt32(sayi);//int <- ToInt32(string)
+
+//+ metin ile birleştir
+Console.WriteLine("a'nın değeri:" + a);//189
+Console.WriteLine("b'nin değeri:" + b);//2024
+Console.WriteLine("sayi'nin değeri:" + sayi);//1234
+
+Console.WriteLine("c'nın değeri:" + c);//??
+Console.WriteLine("d'nın değeri:" + d);//189
+Console.WriteLine("s'nın değeri:" + s);//1234
+
+
+
