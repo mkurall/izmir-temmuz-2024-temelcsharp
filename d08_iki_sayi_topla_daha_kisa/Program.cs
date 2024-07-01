37154d7c9135cf407436b2a296f2f90ee5152f72
Mustafa KURAL <mustafakural@outlook.com>
2024-07-01 15:03:23 +0300

    iki sayı topla insan için daha kısa makine için aynı

---
+++ d08_iki_sayi_topla_daha_kisa/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Bu uygulama klavyeden " + 
+"girilen iki tam sayıyı toplar");
+
+Console.Write("1.Sayıyı Girin:");
+var s1 = Convert.ToInt32(Console.ReadLine());//  int <- ToInt32(string)
+
+Console.Write("2.Sayıyı Girin:");
+var s2 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Sayıların Toplamı = " + (s1 + s2));
