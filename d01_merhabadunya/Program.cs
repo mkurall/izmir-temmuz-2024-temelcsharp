0aa767d1781601225c2fda06111434f9bc7c0cbb
Mustafa KURAL <mustafakural@outlook.com>
2024-07-01 11:40:37 +0300

    first commit

---
+++ d01_merhabadunya/Program.cs
@@ -0,0 +1,11 @@
+﻿//Bu kod ekrana merhaba dünya yazar
+//2.satır
+
+/*
+1.satır
+2.satır
+3.satır
+Turkiye.İzmir.Alsancak
+*/
+
+Console.WriteLine("Merhaba Dünya!");
---
+++ d03_sabitveriler/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Btk Akademi C# Kursu");//metinsel veri
+Console.WriteLine(1981);//tam sayı
+Console.WriteLine(3.14);//ondalıklı sayı
+Console.WriteLine('E');//karakter
+Console.WriteLine(true);//mantıksal veri (true, false)
+Console.WriteLine(false);
+Console.WriteLine("");//boş metin
