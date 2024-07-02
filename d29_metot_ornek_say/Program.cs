6aba844f0d77019c69f9e3055f9d892f1ee3724b
Mustafa KURAL <mustafakural@outlook.com>
2024-07-02 16:29:09 +0300

    say metodu

---
+++ d29_metot_ornek_say/Program.cs
@@ -0,0 +1,15 @@
+﻿//
+void Say(int bas, int son, int art=1)
+{
+    if(bas<son)
+        for (int i = bas; i<son;i+=art)
+        Console.Write($"{i}, ");
+    else
+        for (int i = bas; i>son;i+=art)
+        Console.Write($"{i}, ");
+
+    Console.WriteLine();
+}
+
+Say(50,3,-2);//3, 5, 7
+Say(4,15);//4, 5, 6,
