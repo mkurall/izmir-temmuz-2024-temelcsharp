618c5bad2348c29157342ef04fbbbde95b43dcaf
Mustafa KURAL <mustafakural@outlook.com>
2024-07-02 12:09:06 +0300

    while ile pin kodu

---
+++ d21_while_pin_kodu/Program.cs
@@ -0,0 +1,12 @@
+﻿string pinKodu = "1234";
+string okunanPinKodu = "";//değer atamamız gerek
+
+//Kullanıcı pin kodunu girmeye başlayacak
+while(pinKodu!=okunanPinKodu)//farklı olduğu sürece
+{
+    Console.WriteLine("Pin Kodunu Gir:");
+    okunanPinKodu = Console.ReadLine();
+}
+//1234 != 1234 = False
+Console.WriteLine("TEBRİKLER GİRİŞ BAŞARILI!");
+
