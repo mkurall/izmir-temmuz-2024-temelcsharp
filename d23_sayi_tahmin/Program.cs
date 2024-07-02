66f839e3e51a7c01d243a159ade590e08cab67b3
Mustafa KURAL <mustafakural@outlook.com>
2024-07-02 14:04:51 +0300

    sayı tahmin hak eklendi

--- d23_sayi_tahmin/Program.cs
+++ d23_sayi_tahmin/Program.cs
@@ -1,23 +1,33 @@
 ﻿//rasgele 1-100 arasında sayı üretir
 int rasgele = new Random().Next(1,100);
-
+int hak = 5;
 do
 {
-    Console.Write("Tahminiz nedir?");
+    Console.Write($"Tahminiz nedir? (Kalan Hak = {hak})");
     int tahmin = Convert.ToInt32(Console.ReadLine());
+    hak--;//bir hakkım gitti
 
     if(tahmin == rasgele)
     {
         Console.WriteLine("TEBRİKLER BİLDİNİZ!");
         break;
-    }
-    else if(rasgele > tahmin)
-    {
-        Console.WriteLine("Bilemedin. Daha Yüksek!");
-    }
+    } 
     else
     {
-         Console.WriteLine("Bilemedin. Daha Küçük!");
+        if(hak==0)
+        {
+            Console.WriteLine($"ÜZGÜNÜM BİLEMEDİN! Sayı:{rasgele}");
+            break;
+        }
+
+        if(rasgele > tahmin)
+        {
+            Console.WriteLine("Bilemedin. Daha Yüksek!");
+        }
+        else
+        {
+            Console.WriteLine("Bilemedin. Daha Küçük!");
+        }
     }
 
 } while(true);//sonsuz döngü
