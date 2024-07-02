2781399bfcadd97682a14084df84c7bdea4760c2
Mustafa KURAL <mustafakural@outlook.com>
2024-07-02 15:18:33 +0300

    iki boyutlu diziler

---
+++ d25_string_dizidir/Program.cs
@@ -0,0 +1,8 @@
+﻿string bilgi = "BTK AKADEMİ C# İzmir Yaz Eğitim";
+//char[] bilgi2 = {'B','T','K'};
+
+int uzunluk = bilgi.Length;
+
+for(int i = uzunluk-1;i>=0;i--)
+    Console.Write($"{bilgi[i]}");
+
---
+++ d26_iki_boyutlu_dizi/Program.cs
@@ -0,0 +1,28 @@
+﻿char[,] karakterler = {
+    {'A','H','C','E'},
+    {'R','A','C','E'},
+    {'B','C','H','C'},
+};
+
+int boyut = karakterler.Rank;
+int satSay = karakterler.GetLength(0);
+int sutSay  = karakterler.GetLength(1);
+
+Console.WriteLine($"Boyut: {boyut}");
+Console.WriteLine($"Satırlar: {satSay}");
+Console.WriteLine($"Sütunlar: {sutSay}");
+
+char ara = 'C';
+int sayi = 0;
+
+for(int sat = 0; sat<satSay;sat++)
+{
+    for (int sut = 0; sut < sutSay; sut++)
+    {
+        if(ara == karakterler[sat, sut])
+          sayi++;
+    }
+}
+
+
+Console.WriteLine($"Bulunan '{ara}' sayısı: {sayi}");
