527bb06274e7c259ecae12cbf34379b51748e23e
Mustafa KURAL <mustafakural@outlook.com>
2024-07-02 15:48:04 +0300

    metot ile toplama

--- d14_askere_gidebilir_mi/obj/Debug/net8.0/d14_askere_gidebilir_mi.AssemblyInfo.cs
+++ d14_askere_gidebilir_mi/obj/Debug/net8.0/d14_askere_gidebilir_mi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("d14_askere_gidebilir_mi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07782e53f3729abb748a03ac3ef1d7bee8263c94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2781399bfcadd97682a14084df84c7bdea4760c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("d14_askere_gidebilir_mi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("d14_askere_gidebilir_mi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ d27_metotlar_ile_topla/Program.cs
@@ -0,0 +1,32 @@
+﻿void BilgiVer()//imza (başlık)
+{   //gövde
+    Console.WriteLine("Bu uygulama BTKAKADEMİ C# Kursunda Yazıldı.");
+}
+void Bekle()
+{
+    Console.WriteLine("Uygulamayı sonlandırmak için bir tuşa basın...");
+    Console.ReadKey();
+}
+int Oku(string mesaj)
+{
+    Console.Write(mesaj);
+    int sayi = Convert.ToInt32(Console.ReadLine());
+    return sayi;
+}
+int Islem(int a, int b)
+{
+    int sonuc = a + b;
+    return sonuc;
+}
+
+//ANAPROGRAM BOLUMU
+BilgiVer();//metodu çağırdık
+
+int s1 = Oku("1.Sayıyı Gir:");
+int s2 = Oku("2.Sayıyı Gir:");
+
+int sonuc = Islem(s1, s2);
+
+Console.WriteLine($"İşlem Sonucu = {sonuc}");
+Bekle();
+//ANAPROGRAM SONU
