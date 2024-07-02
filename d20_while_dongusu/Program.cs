ddde2c61cd83680581afd54392e479b495c12438
Mustafa KURAL <mustafakural@outlook.com>
2024-07-02 11:58:28 +0300

    while döngüsü

---
+++ d20_while_dongusu/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("For: ");
+for(int i=1;i<=10;i++)
+{
+    Console.Write($"{i}, ");
+}
+
+Console.WriteLine();//Bir alt satıra geç
+Console.Write("While: ");
+
+int j = 1;
+while(j<=10)
+{
+    Console.Write($"{j}, ");
+    j++;
+}
