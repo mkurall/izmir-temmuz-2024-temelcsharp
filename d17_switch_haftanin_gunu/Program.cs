8460b512dd19446e2fb650e62c0c7dc7fcc7b060
Mustafa KURAL <mustafakural@outlook.com>
2024-07-02 11:02:02 +0300

    switch ile haftanın günü

---
+++ d17_switch_haftanin_gunu/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Write("Haftanın Kaçıncı günü:");
+int gunNo = Convert.ToInt32(Console.ReadLine());
+
+//1-PZT, 2-SAL 3-ÇRŞ......7-PZR
+/*
+if(gunNo == 1)
+    Console.Write("PAZARTESİ");
+else if(gunNo == 2)
+    Console.Write("SALI");
+else if(gunNo == 3)
+    Console.Write("ÇARŞAMBA");
+else if(gunNo == 4)
+    Console.Write("PERŞEMBE");
+else if(gunNo == 5)
+    Console.Write("CUMA");
+else if(gunNo == 6)
+    Console.Write("CUMARTESİ");
+else if(gunNo == 7)
+    Console.Write("PAZAR");
+else 
+    Console.Write("HATALI GÜN!");
+*/
+
+switch(gunNo)
+{
+    case 1:Console.Write("PAZARTESİ");break;
+    case 2:Console.Write("SALI");break;
+    case 3:Console.Write("ÇARŞAMBA");break;
+    case 4:Console.Write("PERŞEMBE");break;
+    case 5:Console.Write("CUMA");break;
+    case 6:Console.Write("CUMARTESİ");break;
+    case 7:Console.Write("PAZAR");break;
+    default:Console.Write("HATALI GÜN!");break;
+}
+
+
+
+
